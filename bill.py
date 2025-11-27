66482272a02fdee22f5029bf8c622793daa1ac36
Krishna H K <khk803123@gmail.com>
2025-11-27 15:39:52 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,13 +1,15 @@
 
 import sys
-if len(sys.argv) != 2:
+if len(sys.argv) == 2:
+    script_name=sys.argv[0]
     units = float(sys.argv[1])
-
 else:
   units=100
+
+
 rate_per_unit = 5
 bill_amount = units * rate_per_unit
 
 print(f"Units Consumed: {units}")
-print(f"Rate per Unit: ₹{rate_per_unit}")
-print(f"Total Electricity Bill: ₹{bill_amount}")
+print(f"Rate per Unit: {rate_per_unit}")
+print(f"Total Electricity Bill: {bill_amount}")
